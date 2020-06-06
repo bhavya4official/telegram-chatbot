95efc936aaa15f7473de74f32983c9e1038ee7e5
Bhavya SIngh <bhavya4official@gmail.com>
2020-06-06 21:07:11 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,13 +1,22 @@
+import re
 from bot import telegram_chatbot
-import gizoogle
 
 bot = telegram_chatbot("config.cfg")
 
+Previous_Date = datetime.datetime.today() - datetime.timedelta(days=6)
+Previous_Date_Formatted = Previous_Date.strftime ('%d/%m/%y') # format the date to dd/mm/yy
+pre_date = str(Previous_Date_Formatted)
+
+f = open("WhatsApp Chat with s3 - whatsapp summary bot.txt", "r")
 
 def make_reply(msg):
     reply = None
     if msg is not None:
-        reply = gizoogle.text(msg)
+        for x in f:
+            if re.search("code",x):
+                if re.search("^[0-9]",x):
+                    reply = x[20:]
+                    break
     return reply
 
 update_id = None
@@ -20,7 +29,7 @@ while True:
             try:
                 message = str(item["message"]["text"])
             except:
-                message = None
+                message = "some exception"
             from_ = item["message"]["from"]["id"]
             reply = make_reply(message)
             bot.send_message(reply, from_)
